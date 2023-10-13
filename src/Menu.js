72e4ee8786ecf1361a34f2b669e05a2c3bee9a4e
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2023-10-13 18:29:21 +0200

    upload

---
+++ src/Menu.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { pizzaData } from ".";
+import { Pizza } from "./Pizza";
+
+export function Menu({ addToCart, handleDelete }) {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza
+                pizzaObj={pizza}
+                key={pizza.name}
+                addToCart={addToCart}
+                onDelete={handleDelete}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+    </main>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Header } from "./Header";
+import { Menu } from "./Menu";
+import { Footer } from "./Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  const [cartItems, setCartItems] = useState([{ name: [], quantity: 0 }]);
+  const handleDelete = (pizzaName) => {
+    const existingPizza = cartItems.find((pizza) => pizza.name === pizzaName);
+
+    if (existingPizza && existingPizza.quantity > 0) {
+      const updatedQuantity = existingPizza.quantity - 1;
+
+      if (updatedQuantity === 0) {
+        const updatedCart = cartItems.filter(
+          (pizza) => pizza.name !== pizzaName
+        );
+        setCartItems(updatedCart);
+      } else {
+        const updatedCart = cartItems.map((pizza) =>
+          pizza.name === pizzaName
+            ? { ...pizza, quantity: updatedQuantity }
+            : pizza
+        );
+        setCartItems(updatedCart);
+      }
+    }
+  };
+
+  const addToCart = (pizzaObj) => {
+    const index = cartItems.findIndex((item) => item.name === pizzaObj.name);
+    if (index !== -1) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[index].quantity++;
+      setCartItems(updatedCartItems);
+    } else {
+      setCartItems([...cartItems, { ...pizzaObj, quantity: 1 }]);
+    }
+  };
+
+  return (
+    <>
+      <Header cartItems={cartItems} handleDelete={handleDelete} />
+      <div className="container">
+        <Menu addToCart={addToCart} handleDelete={handleDelete} />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+// @ts-ignore
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
