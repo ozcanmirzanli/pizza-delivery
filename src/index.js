305db2d62917e7a9eb1645cf07162d089092dbc1
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2023-10-16 10:10:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Footer } from "./Footer";
 
 export const pizzaData = [
   {
-    name: "ocaccia",
+    name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
     photoName: "pizzas/focaccia.jpg",
