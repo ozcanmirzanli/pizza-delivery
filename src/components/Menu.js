c62cbc7f94179c6e10db509c5b5a5d8a454cb69d
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2023-10-18 19:08:33 +0200

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,8 +13,8 @@ export function Menu({ addToCart, handleDelete }) {
       {numPizzas > 0 ? (
         <>
           <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
+            Italian cuisine. 6 creative dishes to choose from. All from our
+            stone oven, all organic, all delicious.
           </p>
 
           <ul className="pizzas">
